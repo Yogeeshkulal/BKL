c82468d8aaf8f1328cff013eac5d16481bae1663
Yogeeshkulal <yogeeshkulal1234@gmail.com>
2025-05-30 22:21:10 +0530

    Initial

--- lib/lab9.dart
+++ lib/lab9.dart
@@ -1,27 +1,43 @@
 import 'package:flutter/material.dart';
-void main()=>runApp(MaterialApp(home:Home()));
-class Home extends StatelessWidget{
-  final product=const[
-    {'name':'Shoes','price':'29.99'},
-    {'name':'Shirt','price':'56'},
-    {'name':'hat','price':'37.8'}
+void main() => runApp(MaterialApp(home: Home()));
+
+class Home extends StatelessWidget {
+  final product = const [
+    {'name': 'Shirt', 'price': '29.99'},
+    {'name': 'Shoes', 'price': '49.99'},
+    {'name': 'Hat', 'price': '15.99'},
   ];
+
   @override
-    Widget build(BuildContext context)=>Scaffold(
-    appBar: AppBar(title:Text('Shop')),
-    body:ListView.builder(
-      itemCount:product.length,
-      itemBuilder:(_,i)=>ListTile(
-        title:Text(product[i]['name']!),
-        subtitle:Text('\$${product[i]['price']}'),
-        trailing:TextButton(
-          onPressed:()=>ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content:Text('${product[i]['name']} added'))),
-            child:Text('Add')
+  Widget build(BuildContext context) => Scaffold(
+    appBar: AppBar(title: Text('Shop')),
+    body: ListView.builder(
+      itemCount: product.length,
+      itemBuilder: (_, i) => ListTile(
+        title: Text(product[i]['name']!),
+        subtitle: Text('\$${product[i]['price']}'),
+        trailing: TextButton(
+          onPressed: () => ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text('${product[i]['name']} added'))),
+          child: Text('Add'),
+        ),
+        onTap: () => Navigator.push(context,
+            MaterialPageRoute(builder: (_) => Detail(product: product[i]))),
       ),
-      ),
-
-
     ),
+  );
+}
+
+class Detail extends StatelessWidget {
+  final Map<String, String> product;
+  Detail({required this.product});
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    appBar: AppBar(title: Text(product['name']!)),
+    body: Center(child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [Text(product['name']!), Text('\$${product['price']}')],
+    )),
   );
 }
